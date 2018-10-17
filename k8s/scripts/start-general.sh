8762f3616aa9d6607aace099f964ffcbf1fdc62a
Lawrence Dudley <lawrencedudley@Parallaxs-MacBook-Pro.local>
2018-10-17 22:27:07 +0100

    add nginx configuration to front the services

--- k8s/scripts/start-general.sh
+++ k8s/scripts/start-general.sh
@@ -8,4 +8,5 @@ sed -i -e "s#NEW_RELIC_LICENSE_KEY=#NEW_RELIC_LICENSE_KEY=$NEWRELIC_LICENSE_KEY#
 sed -i -e "s#NEWRELIC_API_NAME=#NEWRELIC_API_NAME=$SITE_NAME-$SITE_BRANCH-$ENVIRONMENT-API#g" /src/.env
 sed -i -e "s#NEWRELIC_UI_NAME=#NEWRELIC_API_NAME=$SITE_NAME-$SITE_BRANCH-$ENVIRONMENT-UI#g" /src/.env
 sed -i -e "s#MONGODB_PATH=mongodb://localhost:27017/learninglocker_v2#MONGODB_PATH=$MONGODB_PATH#g" /src/.env
+exec /usr/bin/supervisord -n -c /etc/supervisor.conf
 cd /src && pm2 start pm2/all.json --no-daemon
